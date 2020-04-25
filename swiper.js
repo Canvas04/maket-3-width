20cfbe2d844bb2c3c971a7f6b8606e0234bc0692
Ruslan04071996 <ruslan040796@mail.ru>
2020-04-25 19:33:23 +0300

    Отцентрировал логотипы на 768 разрешение

--- swiper.js
+++ swiper.js
@@ -1,2 +1,9 @@
 let paginationContainer = document.querySelector('.swiper-pagination');
 paginationContainer.style.marginBottom = '40px'; 
+
+let hiddenEl = document.querySelectorAll('.logo-of-companies__el_click');
+for (const el of hiddenEl ) {
+el.style.display = 'none'    
+}
+let linkHidden = document.querySelector('.logo-of-companies__link-block_up');
+linkHidden.style.display = 'none';
